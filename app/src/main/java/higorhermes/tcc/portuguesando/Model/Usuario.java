eaea3c9210a6c612ab3d0033cd0b7231ae3ab052
higorbhermes <higorbhermes@gmail.com>
2018-05-30 23:18:07 -0300

    Telas

---
+++ app/src/main/java/higorhermes/tcc/portuguesando/Model/Usuario.java
@@ -0,0 +1,10 @@
+package higorhermes.tcc.portuguesando.Model;
+
+/**
+ * Created by higor on 29/05/18.
+ */
+
+public class Usuario {
+    String nome, idade, cpf, email, telefone, usuario, senha;
+
+}
